6ee079aae467a29126029bcdfbf8452956f1794b
vincent38 <vincent3897@gmail.com>
2017-11-23 23:24:33 +0100

    Added design for informatique

--- resources/views/informatique.blade.php
+++ resources/views/informatique.blade.php
@@ -14,19 +14,34 @@
 
     <a href="{{ route("home") }}">Retour à l'accueil</a>
 
-    <ul>
-      <li>
-        <a href="google-blockly/blockly/html/jeu.php">Didacticiel</a>
-      </li>
-      <?php
-      for($i = 1; $i <= $nbNiveaux; $i++) { ?>
-        <li>
-          <a href="google-blockly/blockly/html/jeu.php?lvl=<?php echo $i;?>">
-            Niveau <?php echo $i; ?>
-          </a>
-        </li>
-      <?php }
-       ?>
+    <div class="main-menu">
 
+      <div class="main-menu-container-info">
+        <h2>Facile</h2>
+        <ul>
+          <li>
+            <a href="google-blockly/blockly/html/jeu.php">Didacticiel</a>
+          </li>
+          <?php
+          for($i = 1; $i <= $nbNiveaux; $i++) { ?>
+            <li>
+              <a href="google-blockly/blockly/html/jeu.php?lvl=<?php echo $i;?>">
+                Niveau <?php echo $i; ?>
+              </a>
+            </li>
+          <?php }
+          ?>
+        </ul>
+      </div> <!--container-->
+
+      <div class="main-menu-container-info">
+        <h2>Médium</h2>
+      </div> <!--container-->
+
+      <div class="main-menu-container-info">
+        <h2>Expert</h2>
+      </div> <!--container-->
+
+    </div> <!-- main menu -->
   </body>
 </html>
