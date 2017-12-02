671b24bafb1bff5341dc7410877d4a9615d5c5f9
Emile Calixte <calixte.emile@gmail.com>
2017-12-02 15:28:23 +0100

    sur mobile c'est mieux sans le viewport mdr

--- resources/views/layouts/headerHome.blade.php
+++ resources/views/layouts/headerHome.blade.php
@@ -2,7 +2,6 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', '') }} - Page d'accueil</title>
     <link rel="stylesheet" href="{{ asset("css/bootstrap.css")}}">
     <link rel="stylesheet" href="{{ asset("css/home.css") }}">
