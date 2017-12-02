bedea228b985e554870fb50ea9407a56365c7995
Emile Calixte <calixte.emile@gmail.com>
2017-12-02 15:27:26 +0100

    changement bannière + viewport

--- resources/views/layouts/headerHome.blade.php
+++ resources/views/layouts/headerHome.blade.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', '') }} - Page d'accueil</title>
     <link rel="stylesheet" href="{{ asset("css/bootstrap.css")}}">
     <link rel="stylesheet" href="{{ asset("css/home.css") }}">
