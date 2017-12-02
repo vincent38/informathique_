e0512de6d4c02057d25427e3d14fb632ccc87c3d
Emile Calixte <calixte.emile@gmail.com>
2017-12-02 18:27:16 +0100

    début css informatique je vais manger allez salut

--- resources/views/informatique.blade.php
+++ resources/views/informatique.blade.php
@@ -1,54 +1,45 @@
+@extends('layouts.headerPages')
+@section('site')
+
 <?php
   $nbNiveaux = 1; // Nombre de niveaux, sans compter le didacticiel
  ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Escape Colle - Kinimi</title>
-    <link rel="stylesheet" href="{{ asset("css/global.css") }}">
-  </head>
-  <body>
-    <h1>Escape Colle</h1>
+<div class="main-menu">
 
-    <a href="{{ route("home") }}">Retour à l'accueil</a>
+  <div class="main-menu-container-info">
+    <h2>Facile</h2>
+    <ul>
+      <li>
+        <a href="google-blockly/blockly/html/jeu.php">Didacticiel</a>
+      </li>
+      <?php
+      for($i = 1; $i <= $nbNiveaux; $i++) { ?>
+        <li>
+          <a href="google-blockly/blockly/html/jeu.php?lvl=<?php echo $i;?>">
+            Niveau <?php echo $i; ?>
+          </a>
+        </li>
+      <?php }
+      ?>
+      <li>En construction !</li>
+    </ul>
+  </div> <!--container-->
 
-    <div class="main-menu">
+  <div class="main-menu-container-info">
+    <h2>Médium</h2>
+    <ul>
+        <li>En construction !</li>
+    </ul>
+  </div> <!--container-->
 
-      <div class="main-menu-container-info">
-        <h2>Facile</h2>
-        <ul>
-          <li>
-            <a href="google-blockly/blockly/html/jeu.php">Didacticiel</a>
-          </li>
-          <?php
-          for($i = 1; $i <= $nbNiveaux; $i++) { ?>
-            <li>
-              <a href="google-blockly/blockly/html/jeu.php?lvl=<?php echo $i;?>">
-                Niveau <?php echo $i; ?>
-              </a>
-            </li>
-          <?php }
-          ?>
-          <li>En construction !</li>
-        </ul>
-      </div> <!--container-->
+  <div class="main-menu-container-info">
+    <h2>Expert</h2>
+    <ul>
+        <li>En construction !</li>
+    </ul>
+  </div> <!--container-->
 
-      <div class="main-menu-container-info">
-        <h2>Médium</h2>
-        <ul>
-            <li>En construction !</li>
-        </ul>
-      </div> <!--container-->
+</div> <!-- main menu -->
 
-      <div class="main-menu-container-info">
-        <h2>Expert</h2>
-        <ul>
-            <li>En construction !</li>
-        </ul>
-      </div> <!--container-->
-
-    </div> <!-- main menu -->
-  </body>
-</html>
+@endsection
