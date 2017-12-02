7296984b529408e89ea6ef5e3c97fa014bbd9797
Emile Calixte <calixte.emile@gmail.com>
2017-12-02 20:57:28 +0100

    css profil OK (utilisateur connecté ou non)

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,21 +1,45 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Profil - Kinimi</title>
-    <link rel="stylesheet" href="{{ asset("css/global.css") }}">
-  </head>
-  <body>
-    <h1>Profil</h1>
+@extends('layouts.headerPages')
+@section('site')
 
-    @if (Route::has('login'))
-      @auth
-      <p>Bienvenue {{ Auth::user()->name }}</p>
-      @else
-      <p>Erreur, vous n'êtes pas connecté.</p>
-      @endauth
-    @endif
+@if (Route::has('login'))
+  @auth
+    <div class="container" id="msgentete">
+      <div class="row">
+        <div class="col-lg-12">
+          <h2>Bienvenue {{ Auth::user()->name }}</h2>
+          <p>Sur cette page, tu peux consulter ton avancement, mais également faire tout un tas d'autres trucs vachement cools.</p>
+        </div> <!-- col -->
+      </div> <!-- row -->
+    </div> <!-- container -->
 
-    <a href="{{ route("home") }}">Retour à l'accueil</a>
-  </body>
-</html>
+
+
+  <!-- UTILISATEUR NON CONNECTE -->
+  @else
+  <div class="container" id="menu">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>Tu n'es pas connecté !</h2>
+        <p>Tu ne peux pas accéder à cette page si tu n'es pas connecté ! Si tu as déjà créé un compte sur {{ config('app.name', '') }}, connecte-toi avec ton identifiant et ton mot de passe. Sinon, tu peux t'inscrire, c'est entièrement gratuit !</p>
+      </div> <!-- col -->
+    </div> <!-- row -->
+    <div class="row" style="margin-bottom: 20px">
+      <div class="col-xs-6">
+        <a href="{{ route('login') }}" class="btn btn-info btn-lg btn-block"><span class="glyphicon glyphicon-log-in"></span> Connexion</a>
+      </div> <!-- col -->
+      <div class="col-xs-6">
+        <a href="{{ route('register') }}" class="btn btn-success btn-lg btn-block"><span class="glyphicon glyphicon-check"></span> Inscription</a>
+      </div> <!-- col -->
+    </div> <!-- row -->
+    <div class="row">
+      <div class="col-xs-12">
+        <a href="{{ route("home") }}" class="btn btn-success btn-lg  btn-block">Retour à l'accueil</a>
+      </div> <!-- col -->
+    </div> <!-- row -->
+  </div> <!-- container -->
+  <!-- FIN UTILISATEUR NON CONNECTE -->
+
+  @endauth
+@endif
+
+@endsection
