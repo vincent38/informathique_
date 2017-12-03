f8a43cecfaa93b93642ade238fbdd0656eed3dec
Emile Calixte <calixte.emile@gmail.com>
2017-12-03 11:37:03 +0100

    Profil : boutons changement infos (c'est incroyable, allez voir ça)

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -8,15 +8,54 @@
         <div class="col-lg-12">
           <h2>Bienvenue {{ $user->name }}</h2>
           <p>Sur cette page, tu peux consulter ton avancement, mais également faire tout un tas d'autres trucs vachement cools.</p>
-          <h3>Votre niveau</h3>
+        </div> <!-- col -->
+      </div> <!-- row -->
+    </div> <!-- container #msgentete -->
+
+
+    <div class="container menu">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <h3>Ton niveau</h3>
           <p>Niveau : {{ $user_lvl[0]->lvl }} - Expérience : {{ $user_lvl[0]->exp }} / {{ $user_lvl[0]->exp*100 }}</p>
-          <h3>Vos badges</h3>
+          <h3>Tes badges</h3>
           @foreach($user_badges as $ub)
             <p>Badge {{ $badges[$ub->id_badge][0]->name }} obtenu le {{ $ub->created_at }}</p>
           @endforeach
+          <p>Obtiens tous les badges en complétant les exercices de mathématiques et en terminant les niveaux d'Escape-Colle !</p>
         </div> <!-- col -->
       </div> <!-- row -->
-    </div> <!-- container -->
+    </div> <!-- container menu -->
+
+    <div class="container menu">
+      <div class="row">
+        <div class="col-lg-12">
+          <h3>Tes informations</h3>
+          <p>Pseudo : </p>
+          <p>Adresse e-mail : </p>
+        </div> <!-- col -->
+        <div class="col-lg-12" id="chgInfo">
+          <span class="btn btn-success" id="btnChgPseudo">Changer de pseudo</span>
+          <span class="btn btn-success" id="btnChgMail">Changer d'adresse e-mail</span>
+          <span class="btn btn-success" id="btnChgPw">Changer de mot de passe</span>
+        </div> <!-- col #chgInfo -->
+        <div class="col-lg-12" id="chgPseudo">
+          <p>PSEUDO</p>
+          <span class="btn btn-primary btn-danger" id="btnChgPseudoCancel"><span class="glyphicon glyphicon-remove"></span> Annuler</span>
+        </div> <!-- col #chgPseudo -->
+        <div class="col-lg-12" id="chgMail">
+          <p>MAIL</p>
+          <span class="btn btn-primary btn-danger" id="btnChgMailCancel"><span class="glyphicon glyphicon-remove"></span> Annuler</span>
+        </div> <!-- col #chgPseudo -->
+        <div class="col-lg-12" id="chgPw">
+          <p>MDP et pas FDP mdr</p>
+          <span class="btn btn-primary btn-danger" id="btnChgPwCancel"><span class="glyphicon glyphicon-remove"></span> Annuler</span>
+        </div> <!-- col #chgPseudo -->
+        <script id="jsChangementProfil" src="{{ asset("js/leChangementCestMaintenant.js")}}"></script>
+      </div> <!-- row -->
+    </div> <!-- container menu -->
+
 
 
 
