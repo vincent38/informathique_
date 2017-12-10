799c1bdcaa78283988b4f7f79196d937839a93fc
vincent38 <vincent3897@gmail.com>
2017-12-10 21:16:21 +0100

    UUID on signup OK

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\DB;
+use Webpatser\Uuid\Uuid;
 
 class RegisterController extends Controller
 {
