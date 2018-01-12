401cfce106a970792143976b221833e2e15fb8dd
Emile Calixte <calixte.emile@gmail.com>
2018-01-12 11:28:25 +0100

    IE marchpa

--- resources/views/layouts/headerHome.blade.php
+++ resources/views/layouts/headerHome.blade.php
@@ -44,7 +44,7 @@
         @if (preg_match('/launchStory/' , Route::current()->getName()))
           <div class=" pull-left header-profil">
             <a class="btn btn-info" style="margin: 0;" href="{{ route("mathematiques") }}">Retour au menu Mathématiques</a>
-            <span>Histoire n°<span id="idh">0</span> - Bonnes réponses : <span id="gh">0</span> - Mauvaises réponses : <span id="bh">0</span> - Temps écoulé : <span id="th">00:00</span></span>
+            <span>Histoire n°<span id="idh">0</span> - Bonnes réponses : <span id="gh">0</span> - Mauvaises réponses : <span id="bh">0</span> - Temps écoulé : <span id="th">00:00</span></spanp>
           </div>
         @endif
         @if (Route::current()->getName() == 'informatique')
@@ -108,11 +108,19 @@
       <![endif]-->
     </header>
 
-    <!--[if ! IE]>
-    <div id="content">
-      @yield ('site')
-    </div>
-    <![endif]-->
+    @if (Route::current()->getName() == 'home')
+      <!--[if ! IE]>
+      <div id="content">
+        @yield ('site')
+      </div>
+      <![endif]-->
+    @endif
+    
+    @if (Route::current()->getName() != 'home')
+      <div id="content">
+        @yield ('site')
+      </div>
+    @endif
 
   </body>
 </html>
