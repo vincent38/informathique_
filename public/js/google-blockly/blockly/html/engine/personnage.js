3187c38acf8a2f7e2d3f46191f1039509caa9cc2
jul <jules.westersang@gmail.com>
2018-01-12 12:20:11 +0100

    correction champ de vision

--- public/js/google-blockly/blockly/html/engine/personnage.js
+++ public/js/google-blockly/blockly/html/engine/personnage.js
@@ -22,34 +22,34 @@
  *
  */
 
-class Personnage{
-    constructor(jsonPersonnage){
+class Personnage {
+    constructor(jsonPersonnage) {
 
-        if(jsonPersonnage.surveillant){
+        if (jsonPersonnage.surveillant) {
             this.surveillant = true;
-            if(jsonPersonnage.CVB){
+            if (jsonPersonnage.CVB) {
                 this.CVB = new Array();
-                for(var i = 0; i < jsonPersonnage.CVB.length; i++){
+                for (var i = 0; i < jsonPersonnage.CVB.length; i++) {
                     this.CVB[i] = new Array(jsonPersonnage.CVB[i][0], jsonPersonnage.CVB[i][1]);
                 }
                 /*this.imgCVG = new Image();
                 this.imgCVG.src */
             }
-            if(jsonPersonnage.CVG){
+            if (jsonPersonnage.CVG) {
                 this.CVG = new Array();
-                for(var i = 0; i < jsonPersonnage.CVG.length; i++){
+                for (var i = 0; i < jsonPersonnage.CVG.length; i++) {
                     this.CVG[i] = new Array(jsonPersonnage.CVG[i][0], jsonPersonnage.CVG[i][1]);
                 }
             }
-            if(jsonPersonnage.CVH){
+            if (jsonPersonnage.CVH) {
                 this.CVH = new Array();
-                for(var i = 0; i < jsonPersonnage.CVH.length; i++){
+                for (var i = 0; i < jsonPersonnage.CVH.length; i++) {
                     this.CVH[i] = new Array(jsonPersonnage.CVH[i][0], jsonPersonnage.CVH[i][1]);
                 }
             }
-            if(jsonPersonnage.CVD){
+            if (jsonPersonnage.CVD) {
                 this.CVD = new Array();
-                for(var i = 0; i < jsonPersonnage.CVD.length; i++){
+                for (var i = 0; i < jsonPersonnage.CVD.length; i++) {
                     this.CVB[i] = new Array(jsonPersonnage.CVD[i][0], jsonPersonnage.CVD[i][1]);
                 }
             }
@@ -63,31 +63,45 @@ class Personnage{
         this.tailleX = jsonPersonnage.tailleX;
         this.tailleY = jsonPersonnage.tailleY;
         this.img = new Image();
-        this.img.src = '../js/google-blockly/blockly/html/resources/images/'+jsonPersonnage.image;
-        if(jsonPersonnage.imageG){this.imgG = new Image(); this.imgG.src = '../js/google-blockly/blockly/html/resources/images/'+jsonPersonnage.imageG || "";}
-        if(jsonPersonnage.imageD){this.imgD = new Image(); this.imgD.src = '../js/google-blockly/blockly/html/resources/images/'+jsonPersonnage.imageD || "";}
-        if(jsonPersonnage.imageH){this.imgH = new Image(); this.imgH.src = '../js/google-blockly/blockly/html/resources/images/'+jsonPersonnage.imageH || "";}
-        if(jsonPersonnage.imageB){this.imgB = new Image(); this.imgB.src = '../js/google-blockly/blockly/html/resources/images/'+jsonPersonnage.imageB || "";}
+        this.img.src = '../js/google-blockly/blockly/html/resources/images/' + jsonPersonnage.image;
+        if (jsonPersonnage.imageG) {
+            this.imgG = new Image();
+            this.imgG.src = '../js/google-blockly/blockly/html/resources/images/' + jsonPersonnage.imageG || "";
+        }
+        if (jsonPersonnage.imageD) {
+            this.imgD = new Image();
+            this.imgD.src = '../js/google-blockly/blockly/html/resources/images/' + jsonPersonnage.imageD || "";
+        }
+        if (jsonPersonnage.imageH) {
+            this.imgH = new Image();
+            this.imgH.src = '../js/google-blockly/blockly/html/resources/images/' + jsonPersonnage.imageH || "";
+        }
+        if (jsonPersonnage.imageB) {
+            this.imgB = new Image();
+            this.imgB.src = '../js/google-blockly/blockly/html/resources/images/' + jsonPersonnage.imageB || "";
+        }
 
         this.affiche();
     }
-    affiche(){
+
+    affiche() {
         var sup = this;
         this.img.onload = function () {
-            scene.context.drawImage(this, sup.x, sup.y, this.tailleX,this.tailleY);
+            scene.context.drawImage(this, sup.x, sup.y, this.tailleX, this.tailleY);
         }
-    }
-    repaint() {
-        scene.context.drawImage(this.img, this.x, this.y, this.tailleX,this.tailleY);
     }
 
-    tick(){
-        eval(this.onTick);
-/*
-        if(typeof this.phase === 'undefined'){
+    repaint() {
+        scene.context.drawImage(this.img, this.x, this.y, this.tailleX, this.tailleY);
+    }
+
+    tick() {
+        //eval(this.onTick);
+
+        if (typeof this.phase === 'undefined') {
             this.phase = 1;
         }
-        switch(this.phase){
+        switch (this.phase) {
             case 1:
                 this.img.src = this.imgG.src;
                 this.tailleX = 40;
@@ -95,32 +109,43 @@ class Personnage{
                 this.testerDetectionHeros(this.CVG);
                 this.phase++;
                 break;
-            case 2:
+            case 6:
                 this.img.src = this.imgH.src;
                 this.tailleX = 50;
                 this.tailleY = 50;
                 this.testerDetectionHeros(this.CVH);
                 this.phase++;
                 break;
-            case 3:
+            case 11:
                 this.img.src = this.imgG.src;
                 this.tailleX = 40;
                 this.tailleY = 40;
                 this.testerDetectionHeros(this.CVG);
                 this.phase++;
                 break;
-            case 4:
+            case 16:
                 this.img.src = this.imgB.src;
-                this.tailleX = 50;this.tailleY = 50;
+                this.tailleX = 50;
+                this.tailleY = 50;
                 this.testerDetectionHeros(this.CVB);
                 this.phase = 1;
                 break;
-        }*/
+            default:
+                if(this.phase > 1 && this.phase < 6){
+                    this.testerDetectionHeros(this.CVG);
+                }else if(this.phase > 6 && this.phase < 11){
+                    this.testerDetectionHeros(this.CVH);
+                }else if(this.phase > 11 && this.phase < 16){
+                    this.testerDetectionHeros(this.CVG);
+                }
+                this.phase++;
+        }
 
     }
-    testerDetectionHeros(CV){
-        for(var i = 0; i < CV.length; i++){
-            if(heros.tabX == CV[i][0] && heros.tabY == CV[i][1]){
+
+    testerDetectionHeros(CV) {
+        for (var i = 0; i < CV.length; i++) {
+            if (heros.tabX == CV[i][0] && heros.tabY == CV[i][1]) {
                 perdu = 1;
                 swal("Repéré", "Le surveillant vous a repéré dans la zone interdite, vous avez perdu", 'error');
             }
@@ -128,7 +153,7 @@ class Personnage{
     }
 }
 
-function setupPersonnages(jsonData, dessins){
+function setupPersonnages(jsonData, dessins) {
     var tabPersonnages = Array();
     if (jsonData.personnages) {
         for (var i = 0; i < jsonData.personnages.length; i++) {
@@ -139,5 +164,5 @@ function setupPersonnages(jsonData, dessins){
     return tabPersonnages;
 }
 
-function test(){
+function test() {
 }
