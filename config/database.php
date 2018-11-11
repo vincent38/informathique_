844fc41bd0cdc81a984606b687820134114052bd
Vincent AUBRIOT <vincent3897@gmail.com>
2018-11-11 23:55:33 +0900

    trial

--- config/database.php
+++ config/database.php
@@ -4,36 +4,6 @@ $DATABASE_URL = parse_url(getenv("DATABASE_URL"));
 
 return [
 
-    // …
-
-    'connections' => [
-
-        // …
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'require',
-        ],
-
-        // …
-
-    ],
-
-    // …
-
-];
-
-
-return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -88,15 +58,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
